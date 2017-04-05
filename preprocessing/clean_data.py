05621998d014a4ae6527d222905d8374252bee72
dsgorthy <derek.gorthy@colorado.edu>
2017-04-04 22:33:05 -0600

    Refactored code into functions for reuse in other functions.

--- preprocessing/clean_data.py
+++ preprocessing/clean_data.py
@@ -12,12 +12,20 @@ import os
 import sys
 
 
-def displayMenu():
-	print("""
-		1. Remove a column
-		9. Exit/Quit
-		""")
+def file_exists(input_file):
+	if not (os.path.isfile(input_file)):
+		print (" ")
+		print ("ERROR: Input file:", input_file) 
+		print ("not found in the raw_data directory.")
+		return 0
+	return 1
 
+
+def display_menu():
+	print("1. Remove a column")
+	print("2. Remove a items of certain category")
+	print("9. Exit/Quit")
+	return
 
 
 def add_line_to_file(line,output_file_name):
@@ -26,30 +34,39 @@ def add_line_to_file(line,output_file_name):
 	return
 
 
-def remove_column(file_name, column, new_file_name):
+def get_column_number(file_name, column):
+
+	f = open(file_name)
+	csv_f = csv.reader(f)
+	column_number = 0
+	column_int = -1
+
+	for row in csv_f:	
+		for header in row:
+			if (column == header):
+				column_int = column_number
+			column_number += 1
+
+	if (column_int == -1):
+		print (" ")
+		print ("ERROR: The column:", column)
+		print ("does not exist.")
+
+	return column_int
+
+
+def remove_column(file_name, column_int, new_file_name):
 
 	if(os.path.isfile(new_file_name)):
 		os.remove(new_file_name)
 
 	first_line = 1
-
 	f = open(file_name)
 	csv_f = csv.reader(f)
-	delete_column = -1
 
 	for row in csv_f:
-		if (first_line):
-			first_line = 0
-			column_number = 0
-			
-			for header in row:
-				if (column == header):
-					delete_column = column_number
-				column_number += 1
-			if (delete_column == -1):
-				print ("The column header --", column , "-- does not exist! Exiting.")
 
-		del row[delete_column]
+		del row[column_int]
 
 		init_str = ""
 
@@ -62,23 +79,35 @@ def remove_column(file_name, column, new_file_name):
 	return
 
 
+def remove_rows_with_element(file_name, remove_string, category_int, new_file_name):
+
+	return
+
+
 
 ans = True
 while ans:
-	displayMenu()
+	display_menu()
 	ans=input("What would you like to do? ")
 
 	if (ans == "1"):
-		column = input("What column would you like to remove? ")
 		input_file = "../raw_data/"+input("What is the name of the input file? ")
-		output_file = "../data_output/"+input("What would you like the output file to be named? ")
+		if (file_exists(input_file)):
+			column_name = input("What column would you like to remove? ")
+			column_int = get_column_number(input_file, column_name)
+			if (column_int != -1):
+				output_file = "../data_output/"+input("What would you like the output file to be named? ")
+				remove_column(input_file,column_int,output_file)	
 
-		if(os.path.isfile(input_file)):
-			remove_column(input_file,column,output_file)
-		else:
-			print (" ")
-			print ("ERROR: Input file:", input_file) 
-			print ("found in the raw_data directory.")	
+	elif (ans == "2"):
+		input_file = "../raw_data/"+input("What is the name of the input file? ")
+		if (file_exists(input_file)):
+			column_name = input("What column would you like to look at? ")
+			column_int = get_column_number(input_file, column_name)
+			if (column_int != -1):
+				data_val = input("What data value do you want to remove? ")
+				output_file = "../data_output/"+input("What would you like the output file to be named? ")
+				remove_rows_with_element(input_file, data_val, column_int, output_file)
 
 	elif (ans == "9"):
 		print ("Goodbye!")
